11d6251984f5abb428f6df18c0f825533924e0d5
wang0704 <zhanpeng00010@163.com>
2019-10-22 17:28:50 +0800

    'test'

--- lpp_test/views.py
+++ lpp_test/views.py
@@ -6,4 +6,4 @@ from common.mymako import render_mako_context
 
 def hello(request):
     # context = {'hello': 'Hello World!'}
-    return render_mako_context(request, '/lpp_test/hello.html')
+    return render_mako_context(request, '/lpp_test/hello.html', dictionary={"ok": "No"})
