ca8594bd128fa0e8156182e9b9f20372127bf4e7
wang0704 <zhanpeng00010@163.com>
2019-10-24 09:49:06 +0800

    token

--- conf/default.py
+++ conf/default.py
@@ -31,7 +31,7 @@ from django.conf.global_settings import *  # noqa
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'test-app-lpp'
-APP_TOKEN = 'faac35d1-d22d-4aa9-aff7-6b39c58cc28d'
+APP_TOKEN = '00639ab9-6f09-4ab1-9fcb-41558233edb7'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'http://paas.bk.com:80'
 
